fc35b21409d3c43875fc0617f94383fa293c29a6
Wef2 <qhrud5725@hanmail.net>
2015-11-25 17:16:36 +0900

    Add Connect, Need to Update

---
+++ app/src/main/java/jnu/mcl/scheduler/connector/DBConnector.java
@@ -0,0 +1,43 @@
+package jnu.mcl.scheduler.connector;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Kim on 2015-11-25.
+ */
+public class DBConnector {
+    String ip = "117.17.102.81";
+    String className = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "scoredb";
+    String un = "sa";
+    String password = "root123";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(className);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
